e033dce3393a2f2e636a62beb9be3329c34f82c0
Alex Vasilkov <alexvasilkov@gmail.com>
2014-09-27 22:20:57 +0700

    Swing UI improvements

--- src/main/groovy/com/alexvasilkov/AndroidSignPlugin.groovy
+++ src/main/groovy/com/alexvasilkov/AndroidSignPlugin.groovy
@@ -1,8 +1,9 @@
 package com.alexvasilkov
 
-import groovy.swing.SwingBuilder
 import org.gradle.api.Plugin
 import org.gradle.api.Project
+import groovy.swing.SwingBuilder
+import java.awt.Component
 
 class AndroidSignPlugin implements Plugin<Project> {
     void apply(Project project) {
@@ -47,12 +48,11 @@ class AndroidSignPlugin implements Plugin<Project> {
             char[] keyPass = ''
 
             if (System.console() == null) {
-                println "Cannot read passwords: System.console() is not available"
                 // Based on https://www.timroes.de/2014/01/19/using-password-prompts-with-gradle-build-files/
                 new SwingBuilder().edt {
                     dialog(
                         modal: true, // Otherwise the build will continue running before you closed the dialog
-                        title: 'Enter password', // Dialog title
+                        title: "${taskName} keystore passwords", // Dialog title
                         alwaysOnTop: true, // pretty much what the name says
                         resizable: false, // Don't allow the user to resize the dialog
                         locationRelativeTo: null, // Place dialog in center of the screen
@@ -60,10 +60,14 @@ class AndroidSignPlugin implements Plugin<Project> {
                         show: true // Let's show it
                         ) {
                         vbox { // Put everything below each other
-                            label(text: "Please enter keystore password:")
-                            storePassInput = passwordField()
-                            label(text: "Please enter key password (leave empty for same as keystore):")
-                            keyPassInput = passwordField()
+                            label(text: "Keystore password:")
+                            passwordField(id: 'storePassInput')
+                            label(text: "(${config.storeFile.getPath()})")
+                            label(text: " ")
+                            label(text: "Key password for '${config.keyAlias}':")
+                            passwordField(id: 'keyPassInput')
+                            label(text: "(leave empty for same as keystore)")
+                            label(text: " ")
                             button(defaultButton: true, text: 'OK', actionPerformed: {
                                 storePass = storePassInput.password; // Set pass variable to value of input field
                                 keyPass = keyPassInput.password;
@@ -76,7 +80,7 @@ class AndroidSignPlugin implements Plugin<Project> {
                 System.console().println("\nKeystore file: ${config.storeFile.getPath()}");
                 storePass = System.console().readPassword("\n${taskName} keystore password:\n")
                 keyPass = System.console().readPassword("${taskName} password for key \"${config.keyAlias}\"" +
-                        " (leave empty if same as keystore password):\n")
+                        " (leave empty for same as keystore):\n")
             }
 
             config.storePassword = new String(storePass)
